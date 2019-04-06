25a8c4a895fd22256c0bf7c3b9f8b43d6003836b
Koen Vantomme <koen.vantomme@gmail.com>
2019-04-06 10:02:45 +0200

    added pub script

---
+++ pdfpythonpub.py
@@ -0,0 +1,15 @@
+# https://pdftables.com/pdf-to-excel-api
+import pdftables_api
+
+# get yoour api key at https://pdftables.com/pdf-to-excel-api 
+api_key= 'xxxxxx'
+
+input = 'xxxx.pdf'
+output= 'xxxx.xls'
+
+c = pdftables_api.Client('1igpxa1x8wf6')
+c.xlsx(input, output) 
+
+#replace c.xlsx with c.csv to convert to CSV
+#replace c.xlsx with c.xml to convert to XML
+#replace c.xlsx with c.html to convert to HTML
